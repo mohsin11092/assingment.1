a15fc5d071ed963aa97e997800562de2f1c2964c
mohsin11092 <mohsintaqvi8@gmail.com>
2024-10-20 17:15:09 +0500

    Add files via upload

---
+++ assignment number 1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{ 
+int a,b;
+printf("Write the first number:");
+scanf("%d", &a);
+printf("/nWrite second number:");
+scanf("%d", &b);
+printf("total of %d and %d is %d:", a,b, a+b);
+return 0;
+
+}
